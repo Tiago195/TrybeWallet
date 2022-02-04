c8b16e7d9dbba2fa6bc37db095981dbf6dd2c4e9
Tiago Meireles <tiagocf475@gmail.com>
2022-02-03 21:29:05 -0300

    reducers feitos, ate o momento

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { EMAIL_LOGIN_TYPE } from '../actions/index';
+
+const INITIAL_STATE = {};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case EMAIL_LOGIN_TYPE:
+    return { ...state, email: action.email };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
